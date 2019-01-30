e75d9073d7461963e2e9bfd6abd6ed9ea56d4152
Shubham Anand <anandshubham.emilence@gmail.com>
2019-01-30 12:46:51 +0530

    Add POST for todos

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 2000;
+var todos = [];
+var todoNextId = 1;
 
-var todos = [{
-    id: 1,
-    description: 'I want to go home!',
-    completed: false
-}, {
-    id: 2,
-    description: 'I need help!',
-    completed: false
-}, {
-    id: 3,
-    description: 'I want to go home for holi celebration!',
-    completed: true
-}];
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Todo API Root');
@@ -40,6 +32,30 @@ app.get('/todos/:id', function (req, res) {
     }
 });
 
+app.post('/todos', function (req, res) {
+    var body = req.body;
+
+    body.id = todoNextId++;
+
+    todos.push(body);
+
+    res.json(body);
+});
+
 app.listen(PORT, function () {
     console.log('Express Server Started on : ' + PORT);
 });
+
+// {
+//     id: 1,
+//     description: 'I want to go home!',
+//     completed: false
+// }, {
+//     id: 2,
+//     description: 'I need your help!',
+//     completed: false
+// }, {
+//     id: 3,
+//     description: 'I want to go home for holi celebration!',
+//     completed: true
+// }
